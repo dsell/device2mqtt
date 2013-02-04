1d50739dde47b25d67ac9dcfa1512a4b0be71aae
Dennis Sell <github@sellfamily.us>
2013-02-04 03:21:04 -0600

    Reworked to function as a daemon

--- device2mqtt.py
+++ device2mqtt.py
@@ -4,7 +4,7 @@
 
 #
 # device
-#	Provides device tracking and alarms
+#    Provides device tracking and alarms
 #
 
 
@@ -12,6 +12,9 @@ __author__ = "Dennis Sell"
 __copyright__ = "Copyright (C) Dennis Sell"
 
 
+APPNAME = "device2mqtt"
+VERSION = "0.8"
+WATCHTOPIC = "/raw/" + APPNAME + "/command"
 
 import sys
 import os
@@ -26,190 +29,80 @@ import pynotify
 import threading
 from config import Config
 import commands
+from daemon import Daemon
+from mqttcore import MQTTClientCore
+from mqttcore import main
 
 
-MYID = socket.gethostname()
-CLIENT_NAME = "device2mqtt[" + MYID + "]"
-CLIENT_VERSION = "0.6"
-CLIENT_TOPIC = "/client/" + CLIENT_NAME + "/" 
-MQTT_TIMEOUT = 60	#seconds
-WATCH_TOPIC = "/device/" + MYID + "/command"
+class MyMQTTClientCore(MQTTClientCore):
+    def __init__(self, appname, clienttype):
+        MQTTClientCore.__init__(self, appname, clienttype)
+        self.clientversion = VERSION
+        self.watchtopic = WATCHTOPIC
+        self.clientversion = VERSION 
+        self.alarmfile = self.cfg.ALARMFILE
 
-LOGFORMAT = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-LOGFILE = "/var/log/device2mqtt.log"
-LOGLEVEL=logging.DEBUG
+        t = threading.Thread(target=self.do_thread_loop)
+        t.start()
 
-#logging.basicConfig(filename=LOGFILE, level=LOGLEVEL, format=LOGFORMAT)
-#TODO might want to add a lock file
-#TODO  need to deal with no config file existing!!!
-#read in configuration file
-homedir = os.path.expanduser("~")
-f = file(homedir + '/.device2mqtt.conf')
-cfg = Config(f)
-MQTT_HOST = cfg.MQTT_HOST
-MQTT_PORT = cfg.MQTT_PORT
-ALARMFILE = cfg.ALARMFILE
-#TODO  need to finish using config values
+    def on_connect(self, mself, obj, rc):
+        MQTTClientCore.on_connect(self, mself, obj, rc)
+        self.mqttc.subscribe(self.watchtopic, 2)
 
-mqtt_connected = False
+    def on_message(self, mself, obj, msg):
+        MQTTClientCore.on_message(self, mself, obj, msg)
+        if ( msg.topic == self.watchtopic ):
+            if ( msg.payload == "stolen" ):
+                p2 = subprocess.Popen( "/usr/bin/amixer set Master 100" )
+                p3 = subprocess.Popen( "/usr/bin/amixer set Master unmute" )
+                p = subprocess.Popen( "mplayer " + self.alarmfile, shell = True )
+                p.wait()
+                print "stolen"
+        else:
+            print "unknown command"
 
+    def do_thread_loop(self):
+        while ( self.running ):
+            if ( self.mqtt_connected ):    
+                    #publish external IP
+                    p = subprocess.Popen( "curl ifconfig.me/ip", shell = True, stdout = subprocess.PIPE )
+                    myip = p.stdout.readline()
+                    self.mqttc.publish( "/device/" + self.clientname + "/ip_ext", myip.strip('\n'), qos=1, retain=True)
 
-#define what happens after connection
-def on_connect( self, obj, rc):
-	global mqtt_connected
-	mqtt_connected = True
-	print "MQTT Connected"
-	logging.info('MQTT connected")
-	mqttc.publish ( "/clients/" + CLIENT_NAME + "/status" , "online", 1, 1 )
-	mqttc.publish( "/clients/" + CLIENT_NAME + "/version", CLIENT_VERSION, 1, 1 )
-#	ip = commands.getoutput("/sbin/ifconfig").split("\n")[1].split()[1][5:]
-	p = subprocess.Popen( "curl ifconfig.me/forwarded", shell = True, stdout = subprocess.PIPE )
-	ip = p.stdout.readline()
-	mqttc.publish( "/clients/" + CLIENT_NAME + "/ip", ip, 1, 1 )
-	mqttc.publish( "/clients/" + CLIENT_NAME + "/pid", os.getpid(), 1, 1 )
-	mqttc.subscribe( WATCH_TOPIC, 2 )
-	mqttc.subscribe( CLIENT_TOPIC + "ping", 2)
+                    #publish internal IP
+                    p = subprocess.Popen( "curl ifconfig.me/forwarded", shell = True, stdout = subprocess.PIPE )
+                    myip = p.stdout.readline()
+                    self.mqttc.publish( "/device/" + self.clientname + "/ip", myip.strip('\n'), qos=1, retain=True)
 
+                    #publish SSID's
+                    nmc = NMClient.Client.new()
+                    devs = nmc.get_devices()
+                    ssids = ""
+                    bssids = ""
+                    for dev in devs:
+                        if dev.get_device_type() == NetworkManager.DeviceType.WIFI:
+                            for ap in dev.get_access_points():
+                                ssids += "\n"
+                                ssids += ap.get_ssid()
+                                bssids += "\n"
+                                bssids += ap.get_bssid()
+                    self.mqttc.publish( "/device/" + self.clientname + "/ssids", ssids, qos=1, retain=True)
+                    self.mqttc.publish( "/device/" + self.clientname + "/bssids", bssids, qos=1, retain=True)
 
-def on_disconnect( self, obj, rc ):
-	pass
-
-
-#On recipt of a message create a pynotification and show it
-def on_message( self, obj, msg):
-	if (( msg.topic == CLIENT_BASE + "ping" ) and ( msg.payload == "request" )):
-		mqttc.publish( CLIENT_BASE + "ping", "response", qos = 1, retain = 0 )
-	else:
-		if ( msg.payload == "stolen" ):
-	#		p2 = subprocess.Popen( "/usr/bin/amixer set Master 100" )
-	#		p3 = subprocess.Popen( "/usr/bin/amixer set Master unmute" )
-			p = subprocess.Popen( "mplayer " + ALARMFILE, shell = True )
-			p.wait()
-			print "stolen"
-		else:
-			print "unknown command"
-
-
-def do_update_loop():
-	global mqtt_connected
-	global running
-	while running:
-		if (mqtt_connected ):
-			#publish external IP
-			p = subprocess.Popen( "curl ifconfig.me/ip", shell = True, stdout = subprocess.PIPE )
-			myip = p.stdout.readline()
-			mqttc.publish( "/device/" + MYID + "/ip_ext", myip.strip('\n'), 1, 1 )
-
-			#publish internal IP
-			p = subprocess.Popen( "curl ifconfig.me/forwarded", shell = True, stdout = subprocess.PIPE )
-			myip = p.stdout.readline()
-			mqttc.publish( "/device/" + MYID + "/ip", myip.strip('\n'), 1, 1 )
-
-			#publish SSID's
-			nmc = NMClient.Client.new()
-			devs = nmc.get_devices()
-			ssids = ""
-			bssids = ""
-			for dev in devs:
-				if dev.get_device_type() == NetworkManager.DeviceType.WIFI:
-					for ap in dev.get_access_points():
-						ssids += "\n"
-						ssids += ap.get_ssid()
-						bssids += "\n"
-						bssids += ap.get_bssid()
-			mqttc.publish( "/device/" + MYID + "/ssids", ssids, 1, 1 )
-			mqttc.publish( "/device/" + MYID + "/bssids", bssids, 1, 1 )
-
-			#publish time of update
-			mqttc.publish( "/device/" + MYID + "/time", time.strftime( "%x %X" ), 1, retain=1 )
-			count = 60
-			while ( mqtt_connected and ( count != 0 )):
-				count -= 1
-				time.sleep(10)
-		else:
-			time.sleep(30)
+                    #publish time of update
+                    self.mqttc.publish( "/device/" + self.clientname + "/time", time.strftime( "%x %X" ), qos=1, retain=True)
+                    count = 60
+                    while ( self.mqtt_connected and ( count != 0 )):
+                        count -= 1
+                        time.sleep(10)
 
 
-def mqtt_connect():
-	rc = 1
-	while ( rc ):
-		print "Attempting connection..."
-		mqttc.will_set("/clients/" + CLIENT_NAME + "/status", "disconnected", 1, 1)
-
-		#define the mqtt callbacks
-		mqttc.on_message = on_message
-		mqttc.on_connect = on_connect
-		mqttc.on_disconnect = on_disconnect
-
-		#connect
-		rc = mqttc.connect( MQTT_HOST, MQTT_PORT, MQTT_TIMEOUT )
-		if rc != 0:
-			logging.info( "Connection failed with error code $s, Retrying", rc )
-			print "Connection failed with error code ", rc, ", Retrying in 30 seconds."
-			time.sleep(30)
-		else:
-			print "Connect initiated OK"
+class MyDaemon(Daemon):
+    def run(self):
+        mqttcore = MyMQTTClientCore(APPNAME, clienttype="type2")
+        mqttcore.main_loop()
 
 
-def mqtt_disconnect():
-	global mqtt_connected
-	if ( mqtt_connected ):
-		mqtt_connected = False 
-		logging.info("MQTT disconnected")
-		print "MQTT Disconnected"
-		mqttc.publish ( "/clients/" + CLIENT_NAME + "/status" , "offline", 1, 1 )
-	mqttc.disconnect()
-
-
-def cleanup(signum, frame):
-	global running
-	running = False
-	mqtt_disconnect()
-	sys.exit(signum)
-
-
-#create an mqtt client
-mqttc = mosquitto.Mosquitto( CLIENT_NAME, clean_session = False )
-
-#trap kill signals including control-c
-signal.signal(signal.SIGTERM, cleanup)
-signal.signal(signal.SIGINT, cleanup)
-
-running = True
-
-t = threading.Thread(target=do_update_loop)
-t.start()
-
-def main_loop():
-	global mqtt_connected
-	mqttc.loop(10)
-	while True:
-		if ( mqtt_connected ):
-			rc = mqttc.loop(10)
-			if rc != 0:	
-				mqtt_disconnect()
-	#			print rc
-				print "Stalling for 20 seconds to allow broker connection to time out."
-				time.sleep(20)
-				mqtt_connect()
-				mqttc.loop(10)
-		pass
-
-
-mqtt_connect()
-main_loop()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    daemon = MyDaemon('/tmp/' + APPNAME + '.pid')
+    main(daemon)
