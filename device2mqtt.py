62e313763b93c4e6cc4a13eef62b28d00e05c49f
Dennis Sell <github@sellfamily.us>
2013-06-08 19:57:40 -0500

    Clean up of different types of clients.

--- device2mqtt.py
+++ device2mqtt.py
@@ -98,7 +98,7 @@ class MyMQTTClientCore(MQTTClientCore):
 
 class MyDaemon(Daemon):
     def run(self):
-        mqttcore = MyMQTTClientCore(APPNAME, clienttype="type2")
+        mqttcore = MyMQTTClientCore(APPNAME, clienttype="multi")
         mqttcore.main_loop()
 
 
